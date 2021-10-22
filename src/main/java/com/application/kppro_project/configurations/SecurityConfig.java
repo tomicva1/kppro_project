9555adc2ad718e9c5558840c9700cd653f722647
vaclavtomicek <tomicva1@uhk.cz>
2021-10-22 16:03:25 +0200

    authentication attempts with DB

--- src/main/java/com/application/kppro_project/configurations/SecurityConfig.java
+++ src/main/java/com/application/kppro_project/configurations/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.application.kppro_project.configurations;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,16 +14,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @EnableWebSecurity
+@PropertySource("classpath:persistence-maria.properties")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Qualifier("userDetailService")
     @Autowired
     UserDetailsService userDetailsService;
+    @Autowired
+    private Environment env;
+
+    //logger for diagnostics
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Autowired
+    private DataSource securityDataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+        //auth.userDetailsService(userDetailsService);
     }
 
     @Override
@@ -46,5 +63,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public DataSource securityDataSource(){
+        //connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        //jdbc driver class
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + env.getProperty("jdbc.url"));
+
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.url"));
+
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    //get environment
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
     }
 }
