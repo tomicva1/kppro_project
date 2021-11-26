bdd412e99bd2d4a3fdd8997bf45a9e1d227e244b
vaclavtomicek <tomicva1@uhk.cz>
2021-11-26 15:00:47 +0100

    added vacation table

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -20,7 +20,24 @@ CREATE TABLE `employee` (
                             city varchar(255) not null,
                             hire_date date null,
                             telephone varchar(255) null
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM;
+
+DROP TABLE IF EXISTS `vacation`;
+CREATE TABLE `vacation` (
+                            id int auto_increment
+                                primary key,
+                            employee_id date not null,
+                            start_date date not null,
+                            end_date date not null,
+                            approved boolean not null,
+                            approvedBy varchar(255) null,
+                            approval_time date null
+) ENGINE=MyISAM;
+
+create index FK6qrwq18c5kq468gvsmwm43cgp
+    on vacation (employee_id);
+
+
 
 --
 -- Inserting data for table `employee`
@@ -28,11 +45,10 @@ CREATE TABLE `employee` (
 
 INSERT INTO `employee`
 VALUES
-(1,'Jan', 'Novak','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user1','bydliste 10','bydliste','2020-01-17', 122333444),
-(2,'Jan', 'Novak','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user2','bydliste 10','bydliste','2020-01-17', 122333444),
-(3,'Jan', 'Novak','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user3','bydliste 10','bydliste','2020-01-17', 122333444),
-(4,'Jan', 'Novak','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user4','bydliste 10','bydliste','2020-01-17', 122333444),
-(5,'Jan', 'Novak','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user5','bydliste 10','bydliste','2020-01-17', 122333444),
-(6,'Jan', 'Novak','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user6','bydliste 10','bydliste','2020-01-17', 122333444);
-
+(1,'Jan', 'Novak','$2a$10$h7B0/27moSbdj47MAjH0iuJDrit6eJARN7ff9nHDJ6umUooauHYn6','USER','user1','bydliste 10','bydliste','2020-01-17', 122333444),
+(2,'Jan1', 'Novak1','$2a$10$h7B0/27moSbdj47MAjH0iuJDrit6eJARN7ff9nHDJ6umUooauHYn6','USER','user2','bydliste 10','bydliste','2020-01-17', 122333444),
+(3,'Jan2', 'Novak2','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user3','bydliste 10','bydliste','2020-01-17', 122333444),
+(4,'Jan3', 'Novak3','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user4','bydliste 10','bydliste','2020-01-17', 122333444),
+(5,'Jan4', 'Novak4','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','USER','user5','bydliste 10','bydliste','2020-01-17', 122333444),
+(6,'Jan8', 'Novak8','$2a$12$DT1PviXFiw889rX4OITUKeyAvCWrzqRDTBf9Gp63cv6Ap1X2YrY9W','MANAGER','manager1','bydliste 10','bydliste','2020-01-17', 122333444);
 
