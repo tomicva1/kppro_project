861db72dc5fc76cef597a45a66f5398863a31ba9
vaclavtomicek <tomicva1@uhk.cz>
2021-12-20 12:16:33 +0100

    Hot fix

---
+++ src/main/java/com/application/kppro_project/configurations/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.application.kppro_project.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().antMatchers("/");
+    }
+}
